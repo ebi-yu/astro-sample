34c3d25b6f6235c6d9721c291153462b08a11207
ebi-yu <ebiDevelopper1088@gmail.com>
2023-12-31 22:57:58 +0900

    💄 React依存の追加

---
+++ packages/astro-sample/src/components/react/Sample.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyComponent() {
+  const style = {
+    backgroundColor: "red",
+    color: "white",
+  };
+
+  return <div style={style}>React Sample Message</div>;
+}
+
+export default MyComponent;
--- packages/astro-sample/src/components/vue/Sample.vue
+++ packages/astro-sample/src/components/vue/Sample.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>test</div>
+  <div id="vue-sample-component">{{ message }}</div>
 </template>
 
 <script setup>
-
+const message = "Vue Sample Message";
 </script>
+
+<style scoped>
+#vue-sample-component {
+  background-color: green;
+  color: white;
+}
+</style>
